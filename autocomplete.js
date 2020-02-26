92eb081b0bc8756b50f5bcd464a380985577707b
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2020-02-26 21:08:31 +0200

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,62 @@
+const createAutoComplete = ({
+    root, 
+    renderOption, 
+    onOptionSelect, 
+    inputValue,
+    fetchData
+}) => {
+    
+    root.innerHTML = `
+      <label><b>Search</b></label>
+      <input class="input"/>
+      <div class="dropdown"> 
+       <div class="dropdown-menu">
+         <div class="dropdown-content results"></div>
+       </div>
+      </div>
+    `;
+
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+    
+    const onInput = async event => {
+        const items = await fetchData(event.target.value);
+    
+        //delete dropdown menu when no results either we delete searching text
+        if(!items.length) {
+            dropdown.classList.remove('is-active'); 
+            return; 
+        }
+        resultsWrapper.innerHTML='';
+        dropdown.classList.add('is-active');
+        for(let item of items) {
+           const option = document.createElement('a'); 
+    
+           
+           option.classList.add('dropdown-item');
+           option.innerHTML = renderOption(item);
+    
+           //changing searching text and closing dropdown menu
+    
+           option.addEventListener('click', () => {
+              dropdown.classList.remove('is-active');
+              input.value = inputValue(item);
+              onOptionSelect(item);
+    
+              
+           });
+    
+           resultsWrapper.appendChild(option);
+       }
+    };
+    input.addEventListener('input', debounce(onInput, 500));
+    
+    //closing menu of movies
+    
+    document.addEventListener('click', event => {
+        if(!root.contains(event.target)) {
+            dropdown.classList.remove('is-active');
+        }
+    });
+};
